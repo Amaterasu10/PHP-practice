8c82807a7d15810e97892dde11999727c3926815
John Hinrich <johnhinrichggalindo@gmail.com>
2020-10-21 18:57:29 +0800

    initials

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <?php include "includes/heading.php"; ?>
+    <title>PHP practice</title>
+</head>
+<body>
+    <header>
+    <?php include "includes/header.php"; ?>
+    </header>
+    
+    <main>
+    </main>
+
+    <footer>
+    <?php include "includes/footer.php"; ?>
+    </footer>
+</body>
+</html>
